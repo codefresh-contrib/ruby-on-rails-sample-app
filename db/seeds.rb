ee14f1945f261cfea337ac14129aec398c39bbcb
Michael Hartl <michael@michaelhartl.com>
2016-10-06 11:51:18 -0700

    Fix spacing

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,12 +11,12 @@ User.create!(name:  "Example User",
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
-  User.create!(name: name,
-              email: email,
-              password:              password,
-              password_confirmation: password,
-              activated: true,
-              activated_at: Time.zone.now)
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
 end
 
 # Microposts
