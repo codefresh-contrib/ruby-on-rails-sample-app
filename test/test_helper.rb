c456b24a42068735b277afb1192e931d2fa22d5e
Michael Hartl <michael@michaelhartl.com>
2018-07-30 17:11:50 -0700

    Bump version number

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -19,7 +19,7 @@ class ActiveSupport::TestCase
 end
 
 class ActionDispatch::IntegrationTest
-  
+
   # Log in as a particular user.
   def log_in_as(user, password: 'password', remember_me: '1')
     post login_path, params: { session: { email: user.email,
